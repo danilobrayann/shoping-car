09c2960a1e283edc8b5aa69d8e03590090dda8c8
danilobrayann <danilobrayann@gmail.com>
2024-05-22 16:28:11 -0300

    feat: new header

--- src/components/SeachBar/seachBar.jsx
+++ src/components/SeachBar/seachBar.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 
 import { FaSearch } from "react-icons/fa";
 export default function SeachBar(){
-
+           /*estado*/ /*função */
+    const [SeachValue,setSeachValue] = useState('');
     return(
         
            
-                <form >
-                    <input type="search" placeholder="buscar produtos" required/>
-                    <button type="submit"><FaSearch /></button>
+                <form className="flex bg-white w-full justify-between gap-5 pr-5">
+                    <input className="p-4 flex-grow border-transparent border-0 outline-none [font-size:0.8rem]" 
+                    type="search" 
+                    value={SeachValue}
+                    placeholder="buscar produtos" 
+                    required 
+                    onChange={({target})=> setSeachValue(target.value)}/>
+                    
+                    <button className="bg-none border-none [font-size:1rem] flex items-center justify-center" type="submit"><FaSearch /></button>
+                    
                 </form>
             
     )
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,9 +2,10 @@ import SeachBar from "../SeachBar/seachBar"
 export default function header(){
 
     return(
-        <header className="bg-yellow-300">
-            <div>
+        <header className="bg-yellow-300 fixed w-full">
+            <div className="flex justify-between items-center p-5">
                 <SeachBar/>
+                <button>cart</button>
             </div>
         </header>
     )
